a33c2df7756b5216d08c8cd77633b5c7a8774d4c
Logan Brown <logan.brown@tylertech.com>
2023-08-31 12:07:55 -0400

    Added timeouts and swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,21 @@ const port = process.env.PORT || 80;
 import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+// Import swagger
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' assert { type: "json" };
+import timeout from 'connect-timeout';
+import bodyParser from 'body-parser';
 
 // View Engine Setup
 app.use(logger('dev'));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
+app.use(timeout('5s'))
+app.use(haltOnTimedout)
 
 app.set('port', port);
 const server = http.createServer(app);
@@ -65,10 +73,21 @@ app.use('/', express.static(path.join(__dirname, '../www/public/'), {
 
 // API Routes
 const baseAPI = '/api/v1';
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.get(`${baseAPI}/status`, (req: any, res: any) => {
     res.setHeader('Cache-Control', 'public, max-age=2.88e+7');
-    res.send({status: 'Online'});
+    res.status(200).send({status: 'Online'});
 });
+
+function haltOnTimedout (req: any, res: any, next: any) {
+    if (req.timedout) {
+        res.status(503).send({status: 'Service Unavailable'});
+    } else {
+        next();
+    }
+  }
 
 // Catch 404 and forward to error handler
 app.use(function(req: any, res: any) {
